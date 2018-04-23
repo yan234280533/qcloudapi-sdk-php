cb1ba03e32692f156a57ccd0eb747710dc7a684e
devinyan <devinyan@tencent.com>
2018-04-23 19:55:57 +0800

    修改配置为永辉的配置

--- examples/ccs/DescribeCluster.php
+++ examples/ccs/DescribeCluster.php
@@ -6,7 +6,7 @@ require_once dirname(__FILE__).'/../../src/QcloudApi/QcloudApi.php';
 $config = array('SecretId'       => getenv('TENCENTCLOUD_SECRET_ID'),
                 'SecretKey'      => getenv('TENCENTCLOUD_SECRET_KEY'),
                 'RequestMethod'  => 'GET',
-                'DefaultRegion'  => 'gz');
+                'DefaultRegion'  => 'yhgz');
 
 $api = QcloudApi::load(QcloudApi::MODULE_CCS, $config);
 
